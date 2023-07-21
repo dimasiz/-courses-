45dc8209ebace7e53bdfd263c943e2fb745a810e
dima shvedko <dima.shvedko.90@mail.ru>
2023-07-21 09:13:52 +0300

    Добавьте файлы проекта.

---
+++ Первая глава/Task 2/Task 2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello ");
+            Console.Write("World ");
+            Console.Write("!!! ");
+            Console.ReadLine();
+        }
+    }
+}
