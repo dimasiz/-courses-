c20c2a2f0372594249c039b7dd1259898a9e3907
dima shvedko <dima.shvedko.90@mail.ru>
2023-07-21 10:00:17 +0300

    первое и второе задание для курсов

--- вторая глава/ConsoleApp1/ConsoleApp1/Program.cs
+++ вторая глава/ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string fullName = "Шведко Дмитрий Иванович";
+            string emailAdrees = "dmitry.Shvedko.mail.ru";
+
+            byte age = 18;
+
+            float programmingBalls = 87.5f;
+            float mathBalls = 67.1f;
+            float physicsBalls = 54.9f;
+
+            float summaBalls = mathBalls + programmingBalls + physicsBalls;
+            float averages = summaBalls / 3;
+
+            Console.WriteLine($"Ф.И.О:{fullName}\nEmail:{emailAdrees}\nВозраст:{age}\nбалы по програмированию:{programmingBalls}\nбалы по математике: " +
+                $"{mathBalls}\nбалы по физике:{physicsBalls}") ;
+            Console.ReadKey();
+
+            Console.WriteLine($"Сумма балов{summaBalls}\nсреднее арифметическое{averages}");
+            Console.ReadKey();
         }
     }
 }
