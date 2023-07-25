dc1187fc1cfaf4242964dd043866da3ef5f0ff35
dima shvedko <dima.shvedko.90@mail.ru>
2023-07-25 14:16:21 +0300

    3_3

---
+++ третья глава/Task 3_3/Task 3_3/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Task_3_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            bool check = true;
+            int divider = 1;
+
+            Console.WriteLine("Введите чило:");
+            number = int.Parse(Console.ReadLine());
+
+            while (check)
+            {
+                if(divider < number - 1)
+                {
+                    divider += 1;
+                }
+
+                if(number % divider == 0)
+                {
+                    Console.WriteLine($"Число {number}:не простое");
+                    check = false;
+                }
+                else
+                {
+                    Console.WriteLine($"Число {number}:простое");
+                    check = false;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
