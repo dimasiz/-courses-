518878dfc35f153d27db499c2b2355f10fb04ecd
dima shvedko <dima.shvedko.90@mail.ru>
2023-07-25 14:28:30 +0300

    задание 4

---
+++ третья глава/Task 3_4/Task 3_4/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task_3_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers;
+            int count;
+            int firstNumber = 0;
+
+            Console.WriteLine("Введите колличевство чисел");
+            count = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите числа через Enter");
+            for (int i = 0; i < count; i++)
+            {
+                numbers = int.Parse(Console.ReadLine());
+                if (firstNumber > numbers)
+                {
+                    firstNumber = numbers;
+                }
+            }
+            Console.WriteLine($"Минимальное число:{firstNumber}");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
