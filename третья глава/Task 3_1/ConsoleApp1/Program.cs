f9f14018528f64624dbe53fd0289ef7582964660
dima shvedko <dima.shvedko.90@mail.ru>
2023-07-25 12:58:34 +0300

    3 глава

---
+++ третья глава/Task 3_1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            Console.Write("Введите число:");
+
+            number =  Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"Число:{number} четное");
+            }
+            else
+            {
+                Console.WriteLine($"Число:{number} не четное");
+            }
+            Console.ReadKey(); 
+
+        }
+    }
+}
