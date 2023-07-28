f2d01b0a26ea7ad126eabf29f0f3268b7e6ad555
dima shvedko <dima.shvedko.90@mail.ru>
2023-07-28 19:13:44 +0300

    4 глава

---
+++ четвертая глава/Task 4_1/Task 4_1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task_4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Веведите колличевство строк:");
+            int lines = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите колличевство столбцев");
+            int columns = int.Parse(Console.ReadLine());
+
+            int [,] matrix = new int[lines, columns];
+
+            Random random = new Random();   
+
+            for (int i = 0; i < lines; i++)
+            {
+                for(int j = 0; j < columns; j++)
+                {
+                    matrix[i, j] = random.Next(0, 101);
+                    Console.Write($"{matrix[i, j],5}") ; 
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
