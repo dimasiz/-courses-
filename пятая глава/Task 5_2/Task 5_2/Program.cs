14388cd112d0fd6cc55a5d1261b59fa1a3f3ec47
dima shvedko <dima.shvedko.90@mail.ru>
2023-07-29 15:45:09 +0300

    Task 5_2

---
+++ пятая глава/Task 5_2/Task 5_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5_2
+{
+    internal class Program
+    {
+        public static string[] VvodText()
+        {
+            Console.WriteLine("Введите текст");
+            string text = Console.ReadLine();
+            string[] words = text.Split(' ');
+            return words;
+        }
+
+        public static void ReversText(string[] words)
+        {
+            string newString = null;
+            Array.Reverse(words);
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                newString += words[i] + " ";
+            }
+            Console.WriteLine(newString);
+        }
+        static void Main(string[] args)
+        {
+            
+            ReversText(VvodText());
+            Console.ReadKey();
+
+        }
+    }
+}
