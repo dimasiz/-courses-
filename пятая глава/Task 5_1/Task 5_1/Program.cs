82f5697ceeeea2547ecbcf4aa79fad3f3665ee7f
dima shvedko <dima.shvedko.90@mail.ru>
2023-07-29 15:36:02 +0300

    задание 5

---
+++ пятая глава/Task 5_1/Task 5_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5_1
+{
+    internal class Program
+    {
+        public static string[] VvodText(string text)
+        {
+            string[] words = text.Split(' ');
+            return words;
+        }
+
+        public static void SplitText(string[] words)
+        {
+            for(int i = 0;i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);    
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст");
+            string text = Console.ReadLine();
+            SplitText(VvodText(text));
+            Console.ReadKey();  
+
+        }
+    }
+}
