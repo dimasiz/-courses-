4b6883bd2f5eb0194dc805c9c8a4c09df2a3b41c
dima shvedko <dima.shvedko.90@mail.ru>
2023-08-18 23:23:24 +0300

    создание 3-ех методов для работы с листом (заполнение 100 рвндомными элементами, сортировка их и удаление ненужных, чтение списков)

---
+++ восьмая глава/Task 8_6_1/Task 8_6_1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_8_6_1
+{
+    internal class Program
+    {
+        public static List<int> RandomList()
+        {
+            int count = 100;
+            Random random = new Random();
+            List<int> numbers = new List<int>();
+
+            for(int i = 0;i < count; i++)
+            {
+                numbers.Add(random.Next(101));
+                
+            }
+            return numbers;
+        }
+
+        public static List<int> ListSort() 
+        {
+            List<int> numbers = RandomList();
+
+            for(int i = 0; i < numbers.Count; i++)
+            {
+                if((numbers[i]>25) && (numbers[i] < 50))
+                {
+                    numbers.RemoveAt(i);
+                }
+            }
+            return numbers;
+        }
+
+        public static void PrintList(List<int> printNumbers)
+        {
+            for(int i = 0; i < printNumbers.Count; i++)
+            {
+                Console.Write(printNumbers[i] + " ");
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 рандомных чисел:");
+            RandomList();
+            PrintList(RandomList());
+
+            Console.WriteLine();
+
+            Console.WriteLine("отсортированый список:");
+            ListSort();
+            PrintList(ListSort());
+
+            Console.ReadLine();
+        }
+    }
+}
