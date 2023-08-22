29c6dad4dc4ed370c495009ce2614cfa04515514
dima shvedko <dima.shvedko.90@mail.ru>
2023-08-22 23:22:26 +0300

    Создание множества которое проверяет наличие уже существуещего числа в списке и говорит об этом пользователю

---
+++ восьмая глава/Task 8_6_3/Task 8_6_3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_8_6_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> numbers = new HashSet<int>();
+            bool flag = true;
+            int number;
+            string enter;
+
+            while (flag)
+            {
+                Console.WriteLine("Введите число");
+                enter = Console.ReadLine();
+
+                if (enter == "")
+                {
+                    flag = false;
+                    break;
+                }
+                number = int.Parse(enter);
+
+                if (numbers.Contains(number))
+                {
+                    
+                    Console.WriteLine("число уже вводилось ранее");
+                }
+                else 
+                {
+                    Console.WriteLine("Ваша число успешно добавлено");
+                }
+
+                numbers.Add(number);
+
+            }
+        }
+    }
+}
