8bc69cfeb879cdc8d99247fa4bb1d12eb7ad7aa7
dima shvedko <dima.shvedko.90@mail.ru>
2023-08-22 22:56:48 +0300

    Был создан справочник с помощью колекции "Dictionary" а так же работа с данными в данной колекции, а именно:  добовление в словарь номера телефона как ключа, добовление имени как содержимого и поиск по ключу содержимого

---
+++ восьмая глава/Task 8_6_2/Task 8_6_2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_8_6_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> guide = new Dictionary<string, string>();
+            bool flag = true;
+            string number;
+            string name;
+
+            while (flag)
+            {
+                Console.WriteLine("Впишите номер телефона");
+                number = Console.ReadLine();
+
+                if (number == "")
+                {
+                    flag = false;
+                    break;
+                }
+
+                Console.WriteLine("Впишите имя");
+                name = Console.ReadLine();
+
+                guide.Add(number, name);
+
+            }
+
+            Console.WriteLine("Введите номер телефона человека которого хотите найти");
+            number = Console.ReadLine();
+
+            if (guide.TryGetValue(number, out name))
+            {
+                Console.WriteLine(name);
+            }
+
+            else if (guide.TryGetValue(number, out name) == false)
+            {
+                Console.WriteLine("Такого номера нету");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
